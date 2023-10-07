f7f7b256055627d750c126d83a3e33faaf46821a
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-10-07 16:46:29 +0900

    FontLoaderをimportする

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 import * as THREE from "./build/three.module.js";
 import { OrbitControls } from "./controls/OrbitControls.js";
+import { FontLoader } from "./build/three.module.js";
 
 //Scene
 const scene = new THREE.Scene();
