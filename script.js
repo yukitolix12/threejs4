6bb0221f5247ed636818c4a562a31b327d0be8c0
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-10-07 16:42:28 +0900

    各変数のsizeをupdateする

--- script.js
+++ script.js
@@ -35,4 +35,18 @@ const animate = () => {
 
     renderer.render(scene, camera);
     window.requestAnimationFrame(animate);
-}
+};
+
+window.addEventListener("resize", () => {
+    //Update sizes
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+
+    //Update camera
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+
+    //Update renderer
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
