87f929b9313ae53eba3dbab80f72bb447396b816
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-10-08 16:01:33 +0900

    materialの変数を作成

--- script.js
+++ script.js
@@ -57,9 +57,9 @@ function(font){
 
     //boxgeometry
     const boxGeometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
-
+    const material = new THREE.MeshNormalMaterial();
     for(let i = 0; i < 100; i++) {
-        
+
     }
 });
 
